d69c63974ff36537fe4c40f7641d953162ae7b26
nick <nick@nowplus.org>
2022-01-14 21:45:56 +0800

    Created constructor

--- ARP_Packet.cpp
+++ ARP_Packet.cpp
@@ -1,1 +1,43 @@
 #include "ARP_Packet.h"
+
+#include <arpa/inet.h>
+
+#include <linux/if_ether.h>
+
+#include <string.h>
+
+#include <iostream>
+/*
+ * Constructor/Destructor
+ */
+
+ARP_Packet::ARP_Packet(unsigned char* packet, int opcode)
+{
+    this->htype = htons(1);
+    this->ptype = htons(ETH_P_IP);
+    
+    this->hlen = HARDWARE_LENGTH;
+    this->plen = PROTOCOL_LENGTH;
+
+    this->opcode = htons(opcode);
+    this->int_opcode = opcode;
+
+    parseAddresses(packet);
+}
+
+/*
+ * Methods
+ */
+
+void ARP_Packet::parseAddresses(unsigned char* packet)
+{
+    // 1 == request, 2 == reply
+    if (int_opcode == 2)
+    {
+        
+    }
+    else
+    {
+        
+    }
+}
--- ARP_Packet.h
+++ ARP_Packet.h
@@ -3,19 +3,8 @@
 
 #include <sys/types.h>
 
-class ARP_Packet
-{
-public:
-
-	// Size in bytes
-	const static int ARP_SIZE = 64;
-	const static int HARDWARE_LENGTH = 6;
-	const static int PROTOCOL_LENGTH = 4;
-	
-
-private:
-
-};
+#define HARDWARE_LENGTH 6
+#define PROTOCOL_LENGTH 4
 
 // ARP header used to send requests
 struct arp_header
@@ -25,11 +14,39 @@ struct arp_header
 	u_int8_t hlen;
 	u_int8_t plen;
 	u_int16_t opcode;
-	u_int8_t sender_mac[ARP_Packet::HARDWARE_LENGTH];
-	u_int8_t sender_ip[ARP_Packet::PROTOCOL_LENGTH];
-	u_int8_t target_mac[ARP_Packet::HARDWARE_LENGTH];
-	u_int8_t target_ip[ARP_Packet::PROTOCOL_LENGTH];
+	u_int8_t sender_mac[HARDWARE_LENGTH];
+	u_int8_t sender_ip[PROTOCOL_LENGTH];
+	u_int8_t target_mac[HARDWARE_LENGTH];
+	u_int8_t target_ip[PROTOCOL_LENGTH];
 } typedef arp_header;
 
+// Used to store and parse data
+class ARP_Packet
+{
+public:
+
+	// Size in bytes
+	const static int ARP_SIZE = 64;
+	ARP_Packet(unsigned char* packet);
+
+private:
+
+	void parseAddresses(unsigned char* packet);
+
+	u_int16_t htype;
+	u_int16_t ptype;
+	u_int8_t hlen;
+	u_int8_t plen;
+	u_int16_t opcode;
+	u_int8_t sender_mac[HARDWARE_LENGTH];
+	u_int8_t sender_ip[PROTOCOL_LENGTH];
+	u_int8_t target_mac[HARDWARE_LENGTH];
+	u_int8_t target_ip[PROTOCOL_LENGTH];
+
+	int int_opcode;
+
+	struct arp_header header;
+
+};
 
 #endif
