65d3cc9869d6433d2b3e22bee724adbb4310c414
nick <nick@nowplus.org>
2022-01-15 16:10:02 +0800

    Created session constructor. Also created printInterface method

--- Session.cpp
+++ Session.cpp
@@ -1,1 +1,44 @@
 #include "Session.h"
+
+#include <iostream>
+#include <stdio.h>
+
+/*
+ * Constructor
+ */
+
+Session::Session(std::string if_name)
+{
+    try
+    {
+        interface = new EthInterface(if_name.c_str());
+        sniffer = new Sniffer(interface->get_if_index());
+
+        // If interface and sniffer created successfully
+        printInterface();
+    }
+    catch (std::runtime_error e)
+    {
+        std::cout << e.what() << std::endl;
+        throw e;
+    }
+}
+
+/*
+ * Methods
+ */
+
+// Just to print the interface details for the user
+void Session::printInterface()
+{
+    unsigned char* mac;
+
+    // Use 2 spaces instead of tab. Looks neater this way
+    std::cout << "Interface: " << interface->get_if_name() << std::endl;
+    std::cout << "  index: " << interface->get_if_index() << std::endl;
+    std::cout << "  ip: " << interface->get_if_ip() << std::endl;
+    
+    mac = interface->get_if_mac();
+    std::cout << "  mac: ";
+    printf("%02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+}
--- Session.h
+++ Session.h
@@ -1,12 +1,24 @@
 #ifndef SESSION_H
 #define SESSION_H
 
+#include <string>
+
+#include "ARP_Packet.h"
+#include "EthInterface.h"
+#include "Sniffer.h"
+
 class Session
 {
 public:
 
+    Session(std::string if_name);
+
 private:
 
+    void printInterface();
+
+    EthInterface* interface;
+    Sniffer* sniffer;
 };
 
 #endif
